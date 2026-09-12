9def4dce0e34204b2d1d953d7a07eb65fd1baef0
andrew2077a <andres.garcia34@udea.edu.co>
2026-09-12 10:21:55 -0500

    terminacion de funciones de bits

--- bits.h
+++ bits.h
@@ -1,10 +1,8 @@
 #ifndef BITS_H
 #define BITS_H
-
-class bits
-{
-public:
-    bits();
-};
-
+int calcularBytesNecesarios(const int posiciones);
+int calcularDesplazamientoBit(const int indicePosicion);
+int calcularIndice(const int fila,const int columna,const int columnas );
+unsigned char obtenerFicha(unsigned char* datos, int indicePosicion);
+void establecerFicha(unsigned char* datos, int indicePosicion, unsigned char valor);
 #endif // BITS_H
