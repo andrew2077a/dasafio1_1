31e07bf6f1eed81e56702218a72313fac83ed5bc
andrew2077a <andres.garcia34@udea.edu.co>
2026-09-12 20:51:28 -0500

    tablero finalizado

--- tablero.h
+++ tablero.h
@@ -1,10 +1,14 @@
 #ifndef TABLERO_H
 #define TABLERO_H
 
-class tablero
-{
-public:
-    tablero();
-};
+void crearTablero(unsigned char*& datos, int& filas, int& columnas, int filasIniciales, int columnasIniciales);
+void destruirTablero(unsigned char*& datos);
+void redimensionarTablero(unsigned char*& datos, int filasViejas, int columnasViejas, int filasNuevas, int columnasNuevas);
+double calcularPorcentajeUso(int filas, int columnas, int bytesReservadosActuales);
+void mostrarTablero(unsigned char* datos, int filas, int columnas);
+void agregarFila(unsigned char*& datos, int& filas, int columnas, int posicion, int& bytesReservadosActuales);
+void eliminarFila(unsigned char*& datos, int& filas, int columnas, int posicion, int& bytesReservados);
+void agregarColumna(unsigned char*& datos, int filas, int& columnas, int posicion, int& bytesReservados);
+void eliminarColumna(unsigned char*& datos, int filas, int& columnas, int posicion, int& bytesReservadosActuales);
 
 #endif // TABLERO_H
